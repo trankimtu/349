c5aace49708ee169eeb13de80956045db7b6a1aa
trankimtu <github@trankimtu.com>
2019-10-08 19:04:09 -0700

    rm
    rm

--- Homework 4/coffeerun-backend/public/scripts/remotedatastore.js
+++ Homework 4/coffeerun-backend/public/scripts/remotedatastore.js
@@ -14,11 +14,21 @@
       console.log(serverResponse);
     });
   };
+  // RemoteDataStore.prototype.getAll = function() {
+  //   $.get(this.serverUrl, function(serverResponse) {
+  //     console.log(serverResponse);
+  //     cb(serverResponse);
+  //   });
+  // };
   RemoteDataStore.prototype.getAll = function() {
-    $.get(this.serverUrl, function(serverResponse) {
-      console.log(serverResponse);
-      cb(serverResponse);
-    });
+    RemoteDataStore.prototype.getAll = function(cb) {
+      $.get(this.serverUrl, function(serverResponse) {
+        console.log(serverResponse);
+        serverResponse.forEach(function(object) {
+          checkList.addRow(object);
+        });
+      });
+    };
   };
   RemoteDataStore.prototype.get = function(key, cb) {
     $.get(this.serverUrl + '/' + key, function(serverResponse) {
@@ -26,12 +36,23 @@
       cb(serverResponse);
     });
   };
+  // RemoteDataStore.prototype.remove = function(key) {
+  //   $.ajax(this.serverUrl + '/' + key, {
+  //     type: 'DELETE'
+  //   });
+  // };
   RemoteDataStore.prototype.remove = function(key) {
-    $.ajax(this.serverUrl + '/' + key, {
-      type: 'DELETE'
+    var getUrl = this.serverUrl;
+    $.get(this.serverUrl, function(serverResponse) {
+      console.log(serverResponse);
+      serverResponse.forEach(function(object) {
+        console.log("ID is " + object.id);
+        $.ajax(getUrl + '/' + object.id, {
+          type: 'DELETE'
+        });
+      });
     });
   };
-
   App.RemoteDataStore = RemoteDataStore;
   window.App = App;
 })(window);
